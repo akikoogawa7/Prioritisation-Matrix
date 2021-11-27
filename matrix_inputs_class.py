0446531190c0460bf8cbf0e276f0bcb828fd5eb1
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-11-27 10:08:03 +0000

    Added to do

---
+++ matrix_inputs_class.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class MatrixInputs():
+    def __init__(self):
+        pass
+
+    #TODO: Insert categories
+    #TODO: Insert target and motivations to understand context of goals
+    #TODO: Insert graphic and ML to classify tasks based on the amount of people who rate a task with weight
