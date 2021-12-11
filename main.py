bc9391612522d708afc86717e89f39191a53c954
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-12-11 16:12:32 +0000

    Cleaned main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 from fastapi import FastAPI
-from typing import List, Optional
-from pydantic import BaseModel
-from datetime import datetime
 
 app = FastAPI()
-
-# create BaseModel Class for Matrix
