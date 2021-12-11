8ae8e3de0a3139dbe63e8e89f36066425ecffbfb
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-12-11 16:36:47 +0000

    Adds errors

--- matrix_inputs_class.py
+++ matrix_inputs_class.py
@@ -1,9 +1,11 @@
+from os import CLD_CONTINUED
 import pandas as pd
 from uuid import UUID, uuid4
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ValidationError, validator
 from typing import Optional, NamedTuple, List, Dict
 from datetime import datetime
 from utils import compute_inputs_json, check_threshold_against_groups_size
+from errors import XYValueError
 
 class MatrixInputs(BaseModel):
     id: UUID = Field(default_factory=uuid4)
@@ -23,36 +25,20 @@ class TasksInputs(BaseModel):
     x_value: float
     y_value: float
 
+    # Checks if x and y values are not more than 100
+    @validator('x_value', 'y_value')
+    @classmethod
+    def must_be_less_than_100(cls, value):
+        if value > 100.0:
+            raise XYValueError(value=value, message='Value should not be more than 100.0')
+        return value
+    
 class UserItemInputs(BaseModel):
     # list_of_tasks: List[TasksInputs] = None
     labels: XYInputs
     metadata: TasksInputs
     
 
-user_1 = UserItemInputs(labels=XYInputs('Importance', 'Effort'), metadata=TasksInputs(task='Do x', x_value=10.0, y_value=10.0))
+user_1 = UserItemInputs(labels=XYInputs('Importance', 'Effort'), metadata=TasksInputs(task='Do x', x_value=101.0, y_value=10.0))
+
 print(f'LABELS: {user_1.labels}, METADATA: {user_1.metadata}')
-
-
-# class MatrixInputs():
-#     def __init__(self, inputs_dataset, agents_group_size=1, threshold=50, matrix=True):
-#         #TODO: Insert categories
-#         x = ('Urgent', 'Less Urgent')
-#         y = ('Important', 'Less Important')
-#         self.categories = [x, y]
-#         self.inputs_dataset = inputs_dataset
-#         self.agents_group_size = agents_group_size
-
-#     def _compute_inputs_dataset(self, inputs_dataset):
-#         #TODO: Insert target and motivations to understand context of goals
-#         compute_inputs_json(inputs_dataset)
-
-#     def _calculate_threshold_against_groups_size(self, agents_groups_size, threshold):
-#         #TODO: Calculate the defining factor for classifying tasks into our 4 categories
-
-#         # Checks if values are within limits and throws error if not
-#         check_threshold_against_groups_size(agents_groups_size, threshold)
-        
-
-#     #TODO: Insert graphic and ML to classify tasks based on the amount of people who rate a task with weight.
-#     # Will the graph size depend on the number of voters?
-#     # Will the division between classes be defined by the threshold? Maybe the threshold complicates things?
