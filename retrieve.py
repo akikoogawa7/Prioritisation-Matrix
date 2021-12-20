dc80a2427a4d2f88a7d27cda4e04f79a1416fa67
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-12-20 17:15:05 +0000

    Updated gitignore

--- retrieve.py
+++ retrieve.py
@@ -4,7 +4,7 @@ local_session = Session()
 
 # returns all data
 users = local_session.query(UserOrm).all()
-matrices = local_session.query(MatrixOutputORM).all()
+matrices = local_session.query(MatrixOutputORM).all()[:3] # limits to 3
 
 for user in users:
     print(user.username)
