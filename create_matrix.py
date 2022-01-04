bcdb7c123f046e4333b0c9b9f95741d84b4e15fc
Akiko Ogawa <akiko.ogawaa@gmail.com>
2022-01-04 22:08:58 +0000

    Moved files to database folder and added apis

--- create_matrix.py
+++
@@ -1,8 +0,0 @@
-from db import MatrixOutputORM, Session
-
-local_session = Session()
-
-new_matrix = MatrixOutputORM(public_key='1234', name='matrix 1')
-
-local_session.add(new_matrix)
-local_session.commit()
--- create_users.py
+++
@@ -1,8 +0,0 @@
-from db import UserOrm, Session
-
-local_session = Session()
-
-new_user = UserOrm(username='akikoogawa', email='akiko.ogawaa@gmail.com', first_name='Akiko', last_name='Ogawa')
-
-local_session.add(new_user)
-local_session.commit()
