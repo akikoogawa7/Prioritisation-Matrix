036a015fc3d16824a42f7638f2105f9d9ed159b0
Akiko Ogawa <akiko.ogawaa@gmail.com>
2022-01-10 22:07:52 +0000

    Updated gitignore

--- bool.py
+++
@@ -1,9 +0,0 @@
-import distutils
-from distutils.util import strtobool
-
-positive = bool(distutils.util.strtobool('Yes'))
-negative = bool(distutils.util.strtobool('No'))
-# distutils.util.strtobool("Yes")
-
-print(positive)
-print(negative)
