b44006d66650da4f0b2a87cce44d1d1e06ff4add
Akiko Ogawa <akiko.ogawaa@gmail.com>
2022-01-10 22:08:48 +0000

    Updated gitignore

--- Makefile
+++
@@ -1,2 +0,0 @@
-run-db:
-	docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres -d postgres
--- errors.py
+++
@@ -1,8 +0,0 @@
-# EXCEPTIONS
-
-class XYValueError(Exception):
-    """ Custom error that is raised when x and y value is more than 100 """
-    def __init__(self, value: str, message: str) -> None:
-        self.value = value
-        self.message = message
-        super().__init__(message)
